9b114e1e2c3baf37a1061ad0b6254b788f7f390d
Kazu Yamamoto <kazu@iij.ad.jp>
2015-09-30 11:47:12 +0900

    initial import.

---
+++ H2O.hs
@@ -0,0 +1,87 @@
+{-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE RecordWildCards #-}
+
+-- https://en.wikipedia.org/wiki/Find_first_set
+-- https://github.com/h2o/h2o/blob/master/lib/http2/scheduler.c
+
+module H2O where
+
+import Data.Bits
+import Data.Array
+import Data.Array.IO
+import Data.IORef
+import Data.Sequence
+import Foreign.C.Types
+
+foreign import ccall unsafe "strings.h flsll"
+    c_fls :: CLong -> CLong
+
+findFirstBitSet :: Int -> Int
+findFirstBitSet = fromIntegral . c_fls . fromIntegral
+
+type Weight = Int
+
+data Entry a = Entry {
+    weight :: Int
+  , deficit :: Int
+  , item :: a
+  } deriving Show
+
+data Queue a = Queue {
+    bitsRef   :: IORef Int
+  , offsetRef :: IORef Int
+  , anchors   :: IOArray Int (Seq (Entry a))
+  }
+
+offsetList :: [Int]
+offsetList = map calc idxs ++ [0]
+  where
+    idxs :: [Double]
+    idxs = [1..256]
+    calc x = round $ (2**(8 - logBase 2 x) * 16128)
+
+offsetTable :: Array Int Int
+offsetTable = listArray (0,256) offsetList
+
+new :: IO (Queue a)
+new = Queue <$> newIORef 0 <*> newIORef 0 <*> newArray (0,63) empty
+
+enqueue :: Entry a -> Queue a -> IO ()
+enqueue Entry{..} Queue{..} = do
+    bits <- readIORef bitsRef
+    offset <- readIORef offsetRef
+    let off' = offsetTable ! (weight - 1) + deficit
+        deficit' = off' `mod` 65536
+        off      = off' `div` 65536
+        ent = Entry weight deficit' item
+        n = 8 * 8 - 1 - off
+        bits' = setBit bits n
+        idx = (offset + off) `div` 64
+    print (weight,off',off,idx,deficit',n,bits')
+    writeIORef bitsRef bits'
+    q <- readArray anchors idx
+    writeArray anchors idx $ ent <| q
+
+dequeue :: Queue a -> IO (Entry a)
+dequeue Queue{..} = do
+    bits <- readIORef bitsRef
+    offset <- readIORef offsetRef
+    let zeroes = 63 - findFirstBitSet bits + 1 -- fixme
+        bits' = shiftL bits zeroes
+    writeIORef bitsRef bits'
+    let off = (offset + zeroes) `mod` 64
+    print (zeroes,off)
+    writeIORef offsetRef off
+    q <- readArray anchors off
+    let q' :> ent = viewr q
+    writeArray anchors off q'
+    return ent
+
+go = do
+    q <- new :: IO (Queue String)
+    enqueue (Entry 201 0 "a") q
+    enqueue (Entry 101 0 "b") q
+    enqueue (Entry 1 0 "c") q
+    dequeue q >>= print
+    dequeue q >>= print
+    dequeue q >>= print
