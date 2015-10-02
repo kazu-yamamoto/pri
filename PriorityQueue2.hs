8a81b0124885944c6271b73b10db7ef8102186bd
Kazu Yamamoto <kazu@iij.ad.jp>
2015-10-02 14:19:19 +0900

    wieght -> priority + heap.

---
+++ PriorityQueue2.hs
@@ -0,0 +1,84 @@
+{-# LANGUAGE BangPatterns #-}
+
+module PriorityQueue2 where
+
+import Data.Array (Array, listArray, (!))
+import Data.Heap (Heap)
+import qualified Data.Heap as H
+
+----------------------------------------------------------------
+
+type Weight = Int
+type Priority = Int
+
+data Ent a = Ent a Weight Priority deriving (Eq,Show)
+
+instance Eq a => Ord (Ent a) where
+    Ent _ _ p1 < Ent _ _ p2 = p1 < p2
+    Ent _ _ p1 <= Ent _ _ p2 = p1 <= p2
+
+data PriorityQueue a = PriorityQueue Int (Heap a)
+
+----------------------------------------------------------------
+
+magicPriority :: Priority
+magicPriority = 0
+
+prioritySteps :: Int
+prioritySteps = 65536 * 63
+
+priorityList :: [Int]
+priorityList = map calc idxs
+  where
+    idxs = [1..256] :: [Double]
+    calc w = round (fromIntegral prioritySteps / w)
+
+priorityTable :: Array Int Int
+priorityTable = listArray (1,256) priorityList
+
+weightToPriority :: Weight -> Priority
+weightToPriority w = priorityTable ! w
+
+----------------------------------------------------------------
+
+newEntry :: a -> Weight -> Ent a
+newEntry x w = Ent x w magicPriority
+
+----------------------------------------------------------------
+
+newQ :: PriorityQueue a
+newQ = PriorityQueue 0 H.empty
+
+enqueue :: Eq a => PriorityQueue (Ent a) -> Ent a -> PriorityQueue (Ent a)
+enqueue (PriorityQueue base heap) (Ent x w p) = PriorityQueue base heap'
+  where
+    !b | p == magicPriority = base -- new entry
+       | otherwise          = p    -- used entry
+    !p' = b + weightToPriority w
+    !ent = Ent x w p'
+    !heap' = H.insert ent heap
+
+dequeue :: Eq a => PriorityQueue (Ent a) -> (Ent a, PriorityQueue (Ent a))
+dequeue (PriorityQueue _ heap) = (ent, PriorityQueue base' heap')
+  where
+    Just (ent@(Ent _ _ p), heap') = H.uncons heap
+    !base' = p `mod` prioritySteps
+
+----------------------------------------------------------------
+
+main :: IO ()
+main = do
+    let q0 = newQ
+        q1 = enqueue q0 (newEntry "a" 201)
+        q2 = enqueue q1 (newEntry "b" 101)
+        q3 = enqueue q2 (newEntry "c"   1)
+    go 1000 q3
+
+go :: Int -> PriorityQueue (Ent String) -> IO ()
+go  0 _ = return ()
+go !n q = do
+    let (x,q') = dequeue q
+    print x
+    let q'' = enqueue q' x
+    go (n - 1) q''
+
