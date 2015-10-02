9f6c34d18d16622171cab91db01ed1a6032a6cee
Kazu Yamamoto <kazu@iij.ad.jp>
2015-10-02 11:06:37 +0900

    STM version.

--- PriorityQueue.hs
+++ PriorityQueue.hs
@@ -8,15 +8,13 @@
 
 module PriorityQueue where
 
+import Control.Concurrent.STM
+import Control.Monad (replicateM)
 import Data.Array (Array, listArray, (!))
-import Data.Array.IO (IOArray, newArray, readArray, writeArray)
+import Data.Array.MArray (newListArray, readArray)
 import Data.Bits (setBit, clearBit, shiftR)
-import Data.IORef (IORef, newIORef, readIORef, writeIORef, modifyIORef')
 import Data.Word (Word64)
 import Foreign.C.Types (CLLong(..))
-
-import RealTimeQueue (RealTimeQueue)
-import qualified RealTimeQueue as RTQ
 
 ----------------------------------------------------------------
 
@@ -29,9 +27,9 @@ data Entry a = Entry {
   } deriving Show
 
 data Queue a = Queue {
-    bitsRef   :: IORef Word64
-  , offsetRef :: IORef Int
-  , anchors   :: IOArray Int (RealTimeQueue (Entry a))
+    bitsRef   :: TVar Word64
+  , offsetRef :: TVar Int
+  , anchors   :: TArray Int (TQueue (Entry a))
   }
 
 ----------------------------------------------------------------
@@ -80,11 +78,16 @@ firstBitSet x = ffs x - 1
 
 ----------------------------------------------------------------
 
-new :: IO (Queue a)
-new = Queue <$> newIORef 0 <*> newIORef 0 <*> newArray (0, bitWidth - 1) RTQ.empty
+new :: STM (Queue a)
+new = Queue <$> newTVar 0
+            <*> newTVar 0
+            <*> newAnchors
+  where
+    newAnchors = replicateM bitWidth newTQueue
+                 >>= newListArray (0, bitWidth - 1)
 
 -- | Enqueuing an entry. Queue is updated.
-enqueue :: Queue a -> Entry a -> IO ()
+enqueue :: Queue a -> Entry a -> STM ()
 enqueue Queue{..} ent = do
     let (idx,deficit') = calcIdxAndDeficit
     offidx <- getOffIdx idx
@@ -97,54 +100,46 @@ enqueue Queue{..} ent = do
         !deficit' = total `mod` deficitSteps
         !idx      = total `div` deficitSteps
     getOffIdx idx = do
-        offset <- readIORef offsetRef
+        offset <- readTVar offsetRef
         let !offidx = relativeIndex idx offset
         return offidx
-    push offidx ent' =
-        updateArray anchors offidx $ \q -> ((), RTQ.enqueue ent' q)
-    updateBits idx = modifyIORef' bitsRef $ \bits -> setBit bits idx
+    push offidx ent' = readArray anchors offidx >>=  \q -> writeTQueue q ent'
+    updateBits idx = modifyTVar' bitsRef $ \bits -> setBit bits idx
 
 -- | Dequeuing an entry. Queue is updated.
-dequeue :: Queue a -> IO (Entry a)
+dequeue :: Queue a -> STM (Entry a)
 dequeue Queue{..} = do
     !idx <- getIdx
     !offidx <- getOffIdx idx
-    ent <- updateArray anchors offidx RTQ.dequeue
+    ent <- readArray anchors offidx >>= readTQueue
     updateOffset offidx
     checkEmpty offidx >>= updateBits idx
     return ent
   where
-    getIdx = firstBitSet <$> readIORef bitsRef
-    getOffIdx idx = relativeIndex idx <$> readIORef offsetRef
-    checkEmpty offidx = RTQ.null <$> readArray anchors offidx
-    updateOffset offset' = writeIORef offsetRef offset'
-    updateBits idx isEmpty = modifyIORef' bitsRef shiftClear
+    getIdx = firstBitSet <$> readTVar bitsRef
+    getOffIdx idx = relativeIndex idx <$> readTVar offsetRef
+    checkEmpty offidx = readArray anchors offidx >>= isEmptyTQueue
+    updateOffset offset' = writeTVar offsetRef offset'
+    updateBits idx isEmpty = modifyTVar' bitsRef shiftClear
       where
         shiftClear bits
           | isEmpty   = clearBit (shiftR bits idx) 0
           | otherwise = shiftR bits idx
 
-updateArray :: IOArray Int a -> Int -> (a -> (b,a)) -> IO b
-updateArray arr idx f = do
-    x <- readArray arr idx
-    let (r,x') = f x
-    writeArray arr idx x'
-    return r
-
 ----------------------------------------------------------------
 
 main :: IO ()
 main = do
-    q <- new :: IO (Queue String)
-    enqueue q (Entry 201 0 "a")
-    enqueue q (Entry 101 0 "b")
-    enqueue q (Entry   1 0 "c")
+    q <- atomically new :: IO (Queue String)
+    atomically $ enqueue q (Entry 201 0 "a")
+    atomically $ enqueue q (Entry 101 0 "b")
+    atomically $ enqueue q (Entry   1 0 "c")
     go 1000 q
 
 go :: Int -> Queue String -> IO ()
 go  0 _ = return ()
 go !n q = do
-    x <- dequeue q
+    x <- atomically $ dequeue q
     print x
-    enqueue q x
+    atomically $ enqueue q x
     go (n - 1) q
